b63335227722f4c387d8afb69f77c3431d97df12
ExtraRandom <crazyblob11@gmail.com>
2018-05-23 12:15:17 +0100

    More folders for real this time

---
+++ Game_Stores_API/ITAD/itad.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+
+def get_plain_from_steam_appid(api_key, app_id):
+    url = "https://api.isthereanydeal.com/v02/game/plain/?key={}&shop=steam&game_id={}".format(api_key, app_id)
+    resp = requests.get(url)
+    data = json.loads(resp.text)
+    if data['.meta']['match'] == False:
+        return "Error", "App doesn't exist on ITAD"
+    else:
+        return data['data']['plain']
+
+
+def get_all_prices(api_key, plain, region="uk"):
+    url = "https://api.isthereanydeal.com/v01/game/prices/?key={}&plains={}&region={}".format(api_key, plain, region)
+    resp = requests.get(url)
+    data = json.loads(resp.text)
+
+    pdata = data['data'][plain]['list']
+
+    return pdata
+
+
+def get_best_price(api_key, plain, region="uk"):
+    url = "https://api.isthereanydeal.com/v01/game/prices/?key={}&plains={}&region={}".format(api_key, plain, region)
+    resp = requests.get(url)
+    data = json.loads(resp.text)
+
+    price = data['data'][plain]['list'][0]['price_new']
+    url = data['data'][plain]['list'][0]['url']
+    shop = data['data'][plain]['list'][0]['shop']['name']
+
+    return price, url, shop
