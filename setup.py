f88076fdc1447021889cbf22a2a561f864dc7aa8
ExtraRandom <crazyblob11@gmail.com>
2018-05-23 22:13:14 +0100

    Even More Setup stuff

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(name='GameStoresAPI',
       license='MIT',
       description='This is where the description would go',
       install_requires=requirements,
-      packages=['Steam'])
+      packages=['steam'])
 
 
 
