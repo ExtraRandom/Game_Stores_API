9e2c693077aff81ec3e741832459399b37e356ba
ExtraRandom <crazyblob11@gmail.com>
2018-05-23 12:08:21 +0100

    More folders

---
+++ Game_Stores_API/__init__.py
@@ -0,0 +1,2 @@
+title = "Game_Stores_API"
+__version__ = '0.0.1'
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+import os, re
+
+requirements = []
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+version = ''
+with open('Games_Stores_API/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+setup(name='game_stores_api',
+      author='Extra_Random',
+      author_email='a',
+      version=version,
+      license='MIT',
+      description='a',
+      install_requires=requirements,
+      packages=['ITAD', 'Origin', 'Playstation', 'Steam', 'Xbox'])
+
+
+
