2322e57fa6fe39f1c59c54272b74df1dedb8aec9
ExtraRandom <crazyblob11@gmail.com>
2018-10-09 01:40:14 +0100

    Test

--- setup.py
+++ setup.py
@@ -7,17 +7,11 @@ with open('requirements.txt') as f:
 setup(name='GameStoresAPI',
       author='Extra_Random',
       author_email='crazyblob11@gmail.com',
-      version='0.0.8.3 - test',
+      version='0.0.8.4 - test',
       license='MIT',
       description='For helping with the collection of pricing and other data from online game stores.',
       install_requires=requirements,
-      packages=['shared',
-                'steam',
-                # 'GameStoresAPI.Origin',
-                # 'GameStoresAPI.Xbox',
-                # 'GameStoresAPI.GMG',
-                'itad',
-                'playstation'])
+      packages=['GameStoresAPI'])
 
 
 
