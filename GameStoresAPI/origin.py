d0cf5187d8e3a009a94e11c81886d03f2133519e
Extra_Random <crazyblob11@gmail.com>
2019-03-13 17:33:31 +0000

    [Origin] Please work

--- GameStoresAPI/origin.py
+++ GameStoresAPI/origin.py
@@ -24,7 +24,7 @@ class Origin:
             )
 
         jdata = json.loads(Shared.get_page_raw(url))
-        jdata["cached_time"] = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        jdata["cached_time"] = time.time()
         jdata["success"] = True
 
         file = os.path.join(os.getcwd(), "origincache.json")
@@ -41,7 +41,7 @@ class Origin:
                 cdata = json.loads(fr.read())
             time_then = cdata["cached_time"]
             time_now = time.time()
-            if (time_now - time_then) >= 60 * 60 * 24: # 1 day
+            if (time_now - time_then) >= 60 * 60 * 24:  # 1 day
                 return Origin.__update_cache_and_return_json()
             else:
                 return cdata
@@ -52,59 +52,53 @@ class Origin:
     @staticmethod
     def search_by_name(display_name):
         jdata = Origin.__get_or_read_cache()
-        if jdata["success"] == False:
-            return {"success": False}
 
-        display_name = str(display_name).lower()
-        # search_items = display_name.split(" ")
+        try:
+            display_name = str(display_name).lower()
+            # search_items = display_name.split(" ")
 
-        ids = []
+            ids = []
 
-        for i in range(jdata['totalCount'] - 1):
-            # print(jdata['offers'][i]['i18n']['displayName'])
-            # print(jdata['offers'][i]['masterTitle'])
+            for i in range(jdata['totalCount'] - 1):
+                # print(jdata['offers'][i]['i18n']['displayName'])
+                # print(jdata['offers'][i]['masterTitle'])
 
-            try:
-                if display_name in str(jdata['offers'][i]['masterTitle']).lower():
-                    ids.append(i)
-                elif display_name in str(jdata['offers'][i]['itemName']).lower():
-                    ids.append(i)
-                elif display_name in str(jdata['offers'][i]['i18n']['displayName']).lower():
-                    ids.append(i)
-            except TypeError:   # print("Type Error")
-                continue
+                try:
+                    if display_name in str(jdata['offers'][i]['masterTitle']).lower():
+                        ids.append(i)
+                    elif display_name in str(jdata['offers'][i]['itemName']).lower():
+                        ids.append(i)
+                    elif display_name in str(jdata['offers'][i]['i18n']['displayName']).lower():
+                        ids.append(i)
+                except TypeError:   # print("Type Error")
+                    continue
 
-        results = []
+            results = []
 
-        for g_id in ids:
-            i_price = jdata['offers'][g_id]['countries']['catalogPrice']
-            if i_price == None:
-                continue  # skip if price is none (i believe this is origin access vault only versions of games)
+            for g_id in ids:
+                i_price = jdata['offers'][g_id]['countries']['catalogPrice']
+                if i_price == None:
+                    continue  # skip if price is none (i believe this is origin access vault only versions of games)
 
-            results.append(
-                {
-                    "name": jdata['offers'][g_id]['i18n']['displayName'],
-                    "desc": jdata['offers'][g_id]['i18n']['shortDescription'],
-                    "price": round(float(i_price), 2),
-                    "currency": jdata['offers'][g_id]['countries']['countryCurrency'],
-                    "dev": jdata['offers'][g_id]['developerFacetKey'],
-                    "pub": jdata['offers'][g_id]['publisherFacetKey'],
-                    "type": jdata['offers'][g_id]['itemType'],
-                    "url_end": jdata['offers'][g_id]['gdpPath']
-                }
-            )
+                results.append(
+                    {
+                        "name": jdata['offers'][g_id]['i18n']['displayName'],
+                        "desc": jdata['offers'][g_id]['i18n']['shortDescription'],
+                        "price": round(float(i_price), 2),
+                        "currency": jdata['offers'][g_id]['countries']['countryCurrency'],
+                        "dev": jdata['offers'][g_id]['developerFacetKey'],
+                        "pub": jdata['offers'][g_id]['publisherFacetKey'],
+                        "type": jdata['offers'][g_id]['itemType'],
+                        "url_end": jdata['offers'][g_id]['gdpPath']
+                    }
+                )
 
-        x = {
-            "success": True,
-            "results": [results]
-        }
-        return x
-
-        res_str = '{"success": true,"results": '
-        res_mid = json.dumps(results)
-        res_end = "}"
-        # return json.loads(res_str + res_mid + res_end)
-
-
+            x = {
+                "success": True,
+                "results": [results]
+            }
+            return x
+        except Exception as e:
+            return {"success": False, "reason": "Args: {}".format(e.args)}
 
 
